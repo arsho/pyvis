06675b800d80567b67ae76e85090d6e7c284ca8c
troyi <ratulbasak93@gmail.com>
2016-12-29 16:36:37 +0600

    Update facedetect.py

--- facedetect.py
+++ facedetect.py
@@ -22,6 +22,7 @@ while True:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     cv2.imshow('img',img)
+    cv2.imwrite('save.png',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
